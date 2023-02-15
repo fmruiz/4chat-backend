8115dd81f126440fecb2f073eb8a75fe638a5d89
fmruiz <x.francoruiz@gmail.com>
2023-02-14 22:19:02 -0300

    feat: add security and standarts middlewares

--- src/setup/setupServer.ts
+++ src/setup/setupServer.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import {
     Application,
     json,
@@ -7,6 +9,13 @@ import {
     NextFunction,
 } from 'express';
 import https from 'https';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import cookieSession from 'cookie-session';
+import HTTP_STATUS from 'http-status-codes';
+import compression from 'compression';
+import 'express-async-errors';
 
 export class ChatServer {
     private app: Application;
@@ -23,9 +32,32 @@ export class ChatServer {
         this.startServer(this.app);
     }
 
-    private securityMiddleware(app: Application): void {}
+    private securityMiddleware(app: Application): void {
+        app.use(
+            cookieSession({
+                name: 'session',
+                keys: ['test1', 'test2'],
+                maxAge: 24 * 7 * 3600000,
+                secure: false,
+            })
+        );
+        app.use(hpp());
+        app.use(helmet());
+        app.use(
+            cors({
+                origin: process.env.URL_LOCAL,
+                credentials: true,
+                optionsSuccessStatus: 200,
+                methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            })
+        );
+    }
 
-    private standartMiddleware(app: Application): void {}
+    private standartMiddleware(app: Application): void {
+        app.use(compression());
+        app.use(json({ limit: '50mb' }));
+        app.use(urlencoded({ extended: true, limit: '50mb' }));
+    }
 
     private routeMiddleware(app: Application): void {}
 
